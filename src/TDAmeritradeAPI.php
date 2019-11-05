f474cecff2135c4fbfd676db27c706047c59429f
Michael Drennen <michaeldrennen74@gmail.com>
2019-11-05 09:01:45 -0700

    Initial commit.

--- src/TDAmeritradeAPI.php
+++ src/TDAmeritradeAPI.php
@@ -2,6 +2,8 @@
 
 namespace MichaelDrennen\TDAmeritradeAPI;
 
+use GuzzleHttp\Client;
+
 class TDAmeritradeAPI {
 
     /**
@@ -10,8 +12,86 @@ class TDAmeritradeAPI {
      */
     protected $oauthConsumerKey;
 
-    public function __construct(string $oauthConsumerKey) {
+    protected $userName;
+    protected $password;
+
+    protected $callbackUrl;
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $guzzle;
+
+    public function __construct( string $oauthConsumerKey, string $userName, string $password, string $callbackUrl ) {
 
         $this->oauthConsumerKey = $oauthConsumerKey;
+        $this->userName         = $userName;
+        $this->password         = $password;
+        $this->callbackUrl      = $callbackUrl;
+        $this->guzzle           = $this->createGuzzleClient();
+
+    }
+
+    /**
+     * @param string|NULL $token
+     * @return \GuzzleHttp\Client
+     */
+    protected function createGuzzleClient( string $token = NULL ): Client {
+
+        $headers             = [];
+        $headers[ 'Accept' ] = 'application/json';
+        if ( $token ):
+            $headers[ 'Authorization' ] = 'Bearer ' . $token;
+        endif;
+
+        $options = [
+
+            'allow_redirects' => [
+                'strict' => TRUE,
+            ],
+            'base_uri'        => 'https://auth.tdameritrade.com',
+            'headers'         => $headers ];
+        return new Client( $options );
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * https://auth.tdameritrade.com/auth?response_type=code&redirect_uri={Callback URL}&client_id={Consumer Key}@AMER.OAUTHAP
+     */
+    public function authenticate() {
+        $html = $this->getHtmlFromAmeritradeLoginPage();
+        $inputs = $this->getInputsFromAmeritradeLoginPage($html);
+
+
+    }
+
+    protected function getHtmlFromAmeritradeLoginPage(){
+        $url = '/auth';
+
+        $options = [
+            'query' => [
+                'response_type' => 'code',
+                'redirect_uri'  => $this->callbackUrl,
+                'client_id'     => $this->oauthConsumerKey . '@AMER.OAUTHAP',
+            ],
+        ];
+
+        $response = $this->guzzle->request( 'GET', $url, $options );
+        $body     = $response->getBody();
+        $contents = $body->getContents();
+
+        return $contents;
+    }
+
+    protected function getInputsFromAmeritradeLoginPage(string $htmlOfLoginPage){
+        var_dump($htmlOfLoginPage);
+        $dom = new \DOMDocument();
+        $dom->loadHTML($htmlOfLoginPage);
+        $inputs = $dom->getElementsByTagName('input');
+        foreach ($inputs as $input):
+            var_dump($input);
+        endforeach;
+        return $inputs;
     }
 }
--- tests/TDAmeritradeTest.php
+++ tests/TDAmeritradeTest.php
@@ -9,11 +9,22 @@ class TDAmeritradeTest extends TestCase {
 
 
     /**
+     * @return TDAmeritradeAPI
+     */
+    protected function getTDAmeritradeAPIInstance(): TDAmeritradeAPI {
+        $oauthConsumerKey = getenv( 'TDAMERITRADE_OAUTH_CONSUMER_KEY' );
+        $userName         = getenv( 'TDAMERITRADE_USERNAME' );
+        $password         = getenv( 'TDAMERITRADE_PASSWORD' );
+        $callbackUrl      = getenv( 'TDAMERITRADE_CALLBACK_URL' );
+        return new TDAmeritradeAPI( $oauthConsumerKey, $userName, $password, $callbackUrl );
+    }
+
+    /**
      * @test
      */
     public function constructorShouldCreateInstance() {
-        $tdAmeritrade = new TDAmeritradeAPI();
-        var_dump( $tdAmeritrade );
+        $tdAmeritrade = $this->getTDAmeritradeAPIInstance();
+        $tdAmeritrade->authenticate();
 
         $this->assertInstanceOf( TDAmeritradeAPI::class, $tdAmeritrade );
     }
